e16655923c3f47afd98f09fda54a470803ee5046
ivannesquick <neskin8@gmail.com>
2021-03-17 21:45:13 +0700

    Initial Commit

---
+++ TheNoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheNoteApp
+//
+//  Created by Neskin Ivan on 17.03.2021.
+//  Copyright © 2021 Neskin Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
